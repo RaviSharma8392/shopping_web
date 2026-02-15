d56e5ce67e5aa892632b67edd3522e69fa9fff38
Ravi <ravisharmabhimtal@gmail.com>
2026-02-15 22:49:09 +0530

    added pwa app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import React, { useEffect } from "react";
+import { BrowserRouter, useLocation } from "react-router-dom";
 import AppRoutes from "./AppRoutes";
 
 import { AuthProvider } from "./userApp/features/auth/context/UserContext";
@@ -7,6 +7,22 @@ import { CartProvider } from "./userApp/features/cart/context/CartContext";
 import { WishlistProvider } from "./userApp/features/wishList/context/WishlistContext";
 import { PopupProvider } from "./userApp/context/SignUpPopContext";
 import { TaruvedaCartProvider } from "./userApp/context/TaruvedaCartContext";
+
+const AppContent = () => {
+  const location = useLocation();
+  const isAdminRoute = location.pathname.startsWith("/admin");
+
+  useEffect(() => {
+    if (!isAdminRoute && "serviceWorker" in navigator) {
+      navigator.serviceWorker
+        .register("/service-worker.js")
+        .then(() => console.log("SW Registered for User"))
+        .catch((err) => console.log("SW Error:", err));
+    }
+  }, [isAdminRoute]);
+
+  return <AppRoutes />;
+};
 
 const App = () => {
   return (
@@ -16,7 +32,7 @@ const App = () => {
           <WishlistProvider>
             <PopupProvider>
               <TaruvedaCartProvider>
-                <AppRoutes />
+                <AppContent />
               </TaruvedaCartProvider>
             </PopupProvider>
           </WishlistProvider>
