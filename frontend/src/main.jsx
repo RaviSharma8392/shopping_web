f045d07c24da817798210158f93efa6230d719e4
Ravi <ravisharmabhimtal@gmail.com>
2026-02-18 08:40:56 +0530

    recover popupcontext

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { HelmetProvider } from "react-helmet-async";
-import { TaruvedaCartProvider } from "./userApp/context/TaruvedaCartContext";
 import App from "./App";
-import { AuthProvider } from "./userApp/features/auth/context/UserContext";
-import { PopupProvider } from "./userApp/context/SignUpPopContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
